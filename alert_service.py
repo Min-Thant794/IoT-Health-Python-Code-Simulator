b629754ec1d6d60c7a3ec37aec98afc6fb69a8ff
Min-Thant794 <minthanthtun29@gmail.com>
2026-01-10 16:15:01 +0800

    Updated in env

--- alert_service.py
+++ alert_service.py
@@ -1,6 +1,14 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-LOGIC_APP_URL = "https://prod-17.centralindia.logic.azure.com:443/workflows/0b44b6ac7a2b47b8a1b907b6c01e7888/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=Dv_saqKlitfU0THr9c_aU_r3npp6hLEKrPZY0ib50uA"
+load_dotenv()
+
+LOGIC_APP_URL = os.getenv("LOGIC_APP_URL")
+
+if not LOGIC_APP_URL:
+    raise ValueError("LOGIC_APP_URL not found in .env file")
+
 
 def send_alert(payload):
     try:
